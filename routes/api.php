a0598a8ce4a6da1c1f641b572557f503fe3bf2a9
naimur404 <naimur404cse@gmail.com>
2025-03-07 03:14:56 +0600

    Add Laravel Sanctum for API authentication and configure routes

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourierController;
+use App\Http\Controllers\CustomerReviewController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::post('/courier-check', [CourierController::class, 'check'])->name('courier.checkApi');
+Route::post('customer-review', [CustomerReviewController::class, 'store'])->name('customer.reviewApi.store');
+Route::get('customer-reviews/{phone}', [CustomerReviewController::class, 'list'])->name('customer.reviewApi.list');
