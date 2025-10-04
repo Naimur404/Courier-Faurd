1dc1ebc0c7e574c6fb3518f440d1fe6ac201fbda
naimur404 <naimur404cse@gmail.com>
2025-10-04 16:56:46 +0600

    Comment out role column addition in users table migration

--- database/migrations/2025_10_04_063557_add_role_to_users_table.php
+++ database/migrations/2025_10_04_063557_add_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'api_user'])->default('api_user')->after('email');
+            // $table->enum('role', ['admin', 'api_user'])->default('api_user')->after('email');
             // $table->string('api_token')->nullable()->unique()->after('remember_token');
             $table->integer('monthly_request_limit')->default(1000)->after('api_token');
             $table->integer('current_month_requests')->default(0)->after('monthly_request_limit');
