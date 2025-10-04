3a1e22a2ede10770071a0f4a4b83ef68506131be
naimur404 <naimur404cse@gmail.com>
2025-10-04 16:55:14 +0600

    Fix migration rollback: retain 'role' column in down method of users table migration

--- database/migrations/2025_10_04_063557_add_role_to_users_table.php
+++ database/migrations/2025_10_04_063557_add_role_to_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn([
-                'role', 
+                // 'role', 
                 // 'api_token', 
                 'monthly_request_limit', 
                 'current_month_requests', 
