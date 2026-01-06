b09e9713bc3bbefc5ef2a1a63c4cae590f87a1c2
Md. Naimur Rahman Akash <naimur404cse@gmail.com>
2026-01-06 17:49:36 +0600

    feat: add refresh action to ListApiUsages and ListCustomers for improved user experience

--- app/Filament/Resources/ApiUsageResource/Pages/ListApiUsages.php
+++ app/Filament/Resources/ApiUsageResource/Pages/ListApiUsages.php
@@ -10,11 +10,21 @@ use Filament\Resources\Pages\ListRecords;
 class ListApiUsages extends ListRecords
 {
     protected static string $resource = ApiUsageResource::class;
+    
+    // Disable query caching for real-time data
+    protected function getTableQueryStringIdentifier(): ?string
+    {
+        return 'api-usages';
+    }
 
     protected function getHeaderActions(): array
     {
         return [
             CreateAction::make(),
+            Actions\Action::make('refresh')
+                ->label('Refresh')
+                ->icon('heroicon-o-arrow-path')
+                ->action(fn () => $this->resetTable()),
         ];
     }
 }
